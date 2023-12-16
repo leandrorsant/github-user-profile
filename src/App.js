1d62de9fed9dba61079457017bb81a9a49ef394d
Leandro <ramosdossantos.leandro@gmail.com>
2023-12-16 12:54:41 -0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import {
+  Home,
+  About,
+  NotFound,
+  Test,
+  Profile,
+  Repo,
+  Activity,
+  Following,
+  Followers,
+  SingleRepo,
+} from './pages';
+import './index.css';
 
-function App() {
+export default function App() {
+  const location = useLocation();
+  const background = Location.state && Location.state.background;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <Routes location={background || location}>
+        <Route path="/" element={<Home />} />
+        <Route exact path="/user" element={<Profile />}>
+          <Route exact path="/user/repo" element={<Repo />}>
+            <Route exact path=":repoid" element={<SingleRepo />} />
+          </Route>
+          <Route exact path="/user/activity" element={<Activity />} />
+          <Route exact path="/user/following" element={<Following />} />
+          <Route exact path="/user/followers" element={<Followers />} />
+        </Route>
+        <Route path="about" element={<About />} />
+        <Route path="test" element={<Test />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      {background && (
+        <Routes>
+          <Route path=":repoid" element={<SingleRepo />} />
+        </Routes>
+      )}
+    </section>
   );
 }
-
-export default App;
